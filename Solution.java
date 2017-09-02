b2bfbd9616029c000b0ea6f8c6cce8ed36c5002d
SiddharthaAnand <siddharthalibra13@gmail.com>
2017-09-02 10:04:16 +0530

    Comments

--- Solution.java
+++ Solution.java
@@ -2,6 +2,7 @@
  * Check if the characters are unique or not. 
  * Assumption: Only Ascii-alphabets lower case present in input.
  * Space decreases if used in this way, bits store the presence of a character.
+ * Input: only consists of a continuous stream of characters and no spaces
  */
 import java.util.*;
 
@@ -12,13 +13,14 @@ class Solution {
 			int setBit = charset & (1 << (input.charAt(i) - 'a'));
 			if (setBit > 0) return false;
 			charset |= (1 << (input.charAt(i) - 'a'));
-			System.out.println(charset);
 		}
 		return true;
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		// Input: only consists of a continuous stream of characters and no spaces
+		//        or else nextLine() needs to be used.
 		String input = sc.next();
 		boolean unique = uniqueCharacters(input);
 		System.out.println(unique);
