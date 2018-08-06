756f39269304c1ffa0e88120853d1f16fbf351de
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-08-06 19:17:57 +0530

    Add input method functionality in utils

--- arrays/Utilities.java
+++ arrays/Utilities.java
@@ -2,6 +2,7 @@
  * Method overloading in action.
  */
 package utilities;
+import java.util.Scanner;
 
 public class Utilities {
 	public static void print(String text, String delimeter, int[] array) {
@@ -19,5 +20,25 @@ public class Utilities {
 
 	public static void print(String text, String delimeter) {
 		System.out.println(text + delimeter);
+	}
+
+	private static Scanner getScanner() {
+		return new Scanner(System.in);
+	}
+
+	public static int takeInputIntegerValue() {
+		Scanner sc = getScanner();
+		return sc.nextInt();
+	}
+
+	// No need to return the value since the values are pushed
+	// in the original array.
+	public static void takeInputArray(int[] array, int len) {
+		Scanner sc = getScanner();
+		int idx = 0;
+		while (len > 0) {
+			len -= 1;
+			array[idx++] = sc.nextInt();
+		}
 	}
 }
