5f265221152d3cd925bfe0ffed4ca5c69be4e026
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-08-08 21:40:02 +0530

    Add emthod to move negatives to the end of array

--- arrays/MoveZero.java
+++ arrays/MoveZero.java
@@ -1,10 +1,31 @@
 /* Move all the zeroes to the end of the given
  * input array.
+ * moveNegativesToEnd(int[] array): This method moves
+ * the negatives to the end of array.
  */
 package arrays;
 import utilities.Utilities;
 
 public class MoveZero {
+	public static void moveNegativesToEnd(int[] array) {
+		int lastIdx = array.length-1;
+		int firstIdx = 0;
+		while (firstIdx < lastIdx) {
+			if (array[firstIdx] < 0 && array[lastIdx] >= 0) {
+				int temp = array[firstIdx];
+				array[firstIdx] = array[lastIdx];
+				array[lastIdx] = temp;
+				firstIdx++; lastIdx--;
+			}
+			else if (array[firstIdx] < 0 && array[lastIdx] <= 0) {
+				lastIdx--;
+			}
+			else if (array[firstIdx] >= 0) {
+				firstIdx++;
+			}
+		}
+	}
+
 	public static void moveZeroesToEnd(int[] array) {
 		int lastIdx = array.length-1;
 		int firstIdx = 0;
@@ -36,7 +57,9 @@ public class MoveZero {
 			array[idx++] = Utilities.takeInputIntegerValue();
 		}
 		moveZeroesToEnd(array);
-		Utilities.print("Array modified", ":", array);
+		Utilities.print("Array modified after moving zeroes to end", ":", array);
+		moveNegativesToEnd(array);
+		Utilities.print("Array modified after moving negatives to end", ":", array);
 
 	}
 }
