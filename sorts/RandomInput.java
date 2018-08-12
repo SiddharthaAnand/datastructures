5a58f778c4290af7f6edbd30160008af15b012b0
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-08-12 12:23:01 +0530

    Add Random generation of inputs

---
+++ sorts/RandomInput.java
@@ -0,0 +1,22 @@
+/* Generate Random numbers for inputs to 
+ * programs.
+ */
+package sorts;
+import java.util.Random;
+
+public class RandomInput {
+	
+	// returns a value [rangeLow, rangeHigh)
+	private static int getRandomValue(int rangeLow, int rangeHigh) {
+		Random rm = new Random();
+		return rm.nextInt(rangeHigh) + rangeLow;
+	}
+
+	public static int[] generateArray(int size, int rangeLow, int rangeHigh) {
+		int[] input = new int[size];
+		for (int idx = 0; idx < size; idx++) {
+			input[idx] = getRandomValue(rangeLow, rangeHigh);
+		}
+		return input;
+	}
+}
