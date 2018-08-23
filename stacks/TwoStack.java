2a04ec1dfc4e3b22b87ee99f05d552229444bfa1
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-08-23 20:42:24 +0530

    Create requirement template

---
+++ stacks/TwoStack.java
@@ -0,0 +1,24 @@
+//https://www.geeksforgeeks.org/?p=18754
+// Implement 2 stacks using an array.
+
+package stacks;
+public class TwoStack {
+	public static void pushFirstStack(int value) {
+
+	}
+
+	public static void pushSecondStack(int value) {
+	}
+
+	public static int popFirstStack() {
+
+	}
+
+	public static int pushSecondStack() {
+		
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
