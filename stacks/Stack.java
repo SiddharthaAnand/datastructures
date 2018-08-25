a49f8d95b6c1a97b85b5cee5f423af1e57e3f744
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-08-25 16:53:22 +0530

    Skeleton for astack

---
+++ stacks/Stack.java
@@ -0,0 +1,28 @@
+// Implement a simple stack using an array
+package stacks;
+
+public class Stack {
+	int top = -1;
+	int capacity;
+	int[] input;
+
+	Stack(int cap) {
+		capacity = cap;
+	}
+	
+	public void pop() {
+
+	}
+
+	public void push(int value) {
+
+	}
+
+	public void peek() {
+
+	}
+
+	public void size() {
+
+	}
+}
