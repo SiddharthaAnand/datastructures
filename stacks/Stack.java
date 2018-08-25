bc5b9be6460edb7ba18a4d75f9903b9f2d8067a9
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-08-25 17:10:02 +0530

    Add print statements and overflow condition change

--- stacks/Stack.java
+++ stacks/Stack.java
@@ -16,11 +16,15 @@ public class Stack {
 
 	public void print() {
 		System.out.println();
+		if (size == 0) {
+			System.out.println("No values in the stack to print!");
+		}
 		for (int idx = 0; idx < size; idx++) {
 			System.out.print(input[idx] + " ");
 		}
 		System.out.println();
 	}
+	
 	public int pop() {
 		int val = Integer.MAX_VALUE;
 		if (top != -1) {
@@ -36,7 +40,7 @@ public class Stack {
 	}
 
 	public void push(int value) {
-		if (top != capacity) {
+		if (top != capacity-1) {
 			top++;
 			input[top] = value;
 			size += 1;
--- stacks/StackHelper.java
+++ stacks/StackHelper.java
@@ -5,20 +5,32 @@ import java.util.Scanner;
 public class StackHelper {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the capacity of the stack: ");
 		int cap = sc.nextInt();
 		Stack st = new Stack(cap);
 		while (true) {
-			System.out.println("Enter 1/2/3 to push/pop/print the stack");
+			System.out.println("Enter 1/2/3/4 to push/pop/print/exit the stack");
 			int val = sc.nextInt();
 			switch(val) {
 				case 1:
+					System.out.print("Enter the value to push: ");
+					val = sc.nextInt();
+					st.push(val);
 					break;
 				case 2:
+					val = st.pop();
+					if (val != Integer.MAX_VALUE) {
+						System.out.println("Popped! " + val);
+					}
 					break;
 				case 3:
+					st.print();
 					break;
+				case 4:
+					System.exit(0);
 				default:
 					break;
 			}
+		}
 	}
 }
