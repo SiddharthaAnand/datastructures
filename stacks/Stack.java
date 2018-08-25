4fdac6716cb1afff42d4bd6aa3b0b03829bc2ecf
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-08-25 17:01:09 +0530

    fix th stack class

--- stacks/Stack.java
+++ stacks/Stack.java
@@ -1,28 +1,56 @@
 // Implement a simple stack using an array
+// SIngle pointer enough to manipulate the stack.
+
 package stacks;
 
 public class Stack {
 	int top = -1;
 	int capacity;
+	int size = 0;
 	int[] input;
 
 	Stack(int cap) {
 		capacity = cap;
+		input = new int[capacity];
 	}
-	
-	public void pop() {
 
+	public int pop() {
+		int val = Integer.MAX_VALUE;
+		if (top != -1) {
+			val = input[top];
+			input[top] = 0;
+			top--;
+			size -= 1;
+		}
+		else {
+			System.out.println("Stack underflow condition");
+		}
+		return val;
 	}
 
 	public void push(int value) {
-
+		if (top != capacity) {
+			top++;
+			input[top] = value;
+			size += 1;
+		}
+		else {
+			System.out.println("Stack overflow condition");
+		}
 	}
 
-	public void peek() {
-
+	public int peek() {
+		int val = Integer.MAX_VALUE;
+		if (top != -1 && top != capacity) {
+			val = input[top];
+		}
+		else {
+			System.out.println("Stack overflow/underflow condition");
+		}
+		return val;
 	}
 
-	public void size() {
-
+	public int size() {
+		return size;
 	}
 }
