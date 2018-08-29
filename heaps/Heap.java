66f0c6a5870588ce6a24bae0f9e49befca6b310d
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-08-29 23:26:45 +0530

    Create skeleton fpr heap

---
+++ heaps/Heap.java
@@ -0,0 +1,22 @@
+// Class which creates a heap.
+package heaps;
+
+class Heap {
+	
+	int[] heap;
+	Heap() {
+
+	}
+
+	public int getMax() {
+
+	}
+
+	public void heapify() {
+
+	}
+
+	public void createHeap() {
+
+	}
+}
