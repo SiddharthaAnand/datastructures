352a19b0c991cb4c40a6a1bfaa2f759341c1668f
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-08-30 23:03:27 +0530

    Create heap ndoe

--- heaps/Heap.java
+++ heaps/Heap.java
@@ -1,10 +1,30 @@
 // Class which creates a heap.
+// Keep a max-heap.
 package heaps;
+
+class HeapNode {
+	int value;
+	int priority = -1;
+	boolean pQueue = false;
+	HeapNode left;
+	HeapNode right;
+
+	HeapNode(int v, int p) {
+		priority = p;
+		value = v;
+		pQueue = true;
+	}
+
+	HeapNode(int v) {
+		value = v;
+	}
+}
 
 class Heap {
 	
 	int[] heap;
 	int size;
+	
 	Heap(int[] input) {
 		size = input.length;
 		heap = input;
