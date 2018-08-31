877ed37f25d01efa2ce34d98bba809cce8baf564
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-08-31 12:14:28 +0530

    Implement heapsort

---
+++ heaps/HeapSort.java
@@ -0,0 +1,54 @@
+/* Class to implement heap sort using already 
+ * created Heap.java class.
+ */
+package heaps;
+import heaps.Heap;
+import java.util.Random;
+import java.util.Scanner;
+
+public class HeapSort {
+	
+	private static void doHeapSort(int[] input) {		
+		Heap hp = new Heap(input);
+		hp.printHeap();
+		hp.createHeap();
+		int size = input.length - 1;
+
+		for (int idx = 0; idx < input.length; idx++) {
+			int temp = hp.array[0];
+			hp.array[0] = hp.array[size];
+			hp.array[size] = temp;
+			size--;
+			hp.capacity--;
+			hp.heapify(0);
+		}
+		print(input);
+
+	}
+
+	private static void print(int[] input) {
+		System.out.println();
+		for (int idx = 0; idx < input.length; idx++) {
+			System.out.print(input[idx] + " ");
+		}
+		System.out.println();
+	}
+	private static int[] getArray(int size, int maxValue) {
+		int[] input = new int[size];
+		Random rm = new Random();
+		for (int idx = 0; idx < size; idx++) {
+			input[idx] = rm.nextInt(maxValue + 1);
+		}
+		return input;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Enter the size of the array: ");
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		System.out.println("Enter the maximum value for the array: ");
+		int maxValue = sc.nextInt();
+		int[] input = getArray(size, maxValue);
+		doHeapSort(input);
+	}
+}
