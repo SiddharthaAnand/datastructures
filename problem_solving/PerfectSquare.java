4a95577cc9d351d19439d5340cef29afff2e6336
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-09-07 19:53:08 +0530

    check for perfect square

---
+++ problem_solving/PerfectSquare.java
@@ -0,0 +1,26 @@
+/* URL: https://practice.geeksforgeeks.org/problems/check-if-a-number-is-perfect-square/0/?track=SP-Misc
+ * Find if a given number is perfect square or not.
+ * Helpful link: https://www.geeksforgeeks.org/rounding-off-errors-java/
+ */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class PerfectSquare {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		while (N > 0) {
+		    N -= 1;
+		    int val = sc.nextInt();
+		    double srt = Math.sqrt(val);
+		    if (srt == Math.floor(Math.sqrt(val))) {
+		        System.out.println(1);
+		    }
+		    else {
+		        System.out.println(0);
+		    }
+		}
+	}
+}
