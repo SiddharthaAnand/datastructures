d7abd1ddeffde12921a3f290eb5fbc6ee9408ddc
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-09-07 23:47:53 +0530

    gcd of array

---
+++ problem_solving/GCDOfArray.java
@@ -0,0 +1,36 @@
+/* URL: https://practice.geeksforgeeks.org/problems/gcd-of-array/0/?track=SP-Misc
+ *
+ */
+
+package problem_solving;
+import java.util.Scanner;
+
+public class GCDOfArray {
+	private static void print() {
+
+	}
+	
+	private static int findGCD(int a, int b) {
+		if (a == 0)
+			return b;
+		return findGCD(b % a, a);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		int idx = 0;
+		int array[] = new int[input];
+		while (input > 0) {
+			int val = sc.nextInt();
+			array[idx] = val;
+			idx++;
+			input--;
+		}
+		int gcd = array[0];
+		for (int i = 1; i < array.length; i++) {
+			gcd = findGCD(gcd, array[i]);
+		}
+		System.out.println("GCD of the array: " + gcd);
+	}
+}
