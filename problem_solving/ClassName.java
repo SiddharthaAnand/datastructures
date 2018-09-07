102ceff0d6a9a3470f1424aea9ac0c3f87e15c6c
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-09-07 21:49:45 +0530

    Create skeleton for other classes

---
+++ problem_solving/ClassName.java
@@ -0,0 +1,17 @@
+/* URL:
+ *
+ */
+
+package name;
+import java.util.Scanner;
+
+public class ClassName {
+	private static void print() {
+
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+	}
+}
