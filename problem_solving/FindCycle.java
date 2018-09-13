a61d1041893afbd45a39997f72748943ea2a4794
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-09-13 13:31:43 +0530

    Find cycle in graph using DFS

--- problem_solving/FindCycle.java
+++ problem_solving/FindCycle.java
@@ -1,13 +1,75 @@
 package problem_solving;
 import java.util.Scanner;
 import java.io.File;
+import java.util.List;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ArrayList;
 
-public class Graph {
-	int nodeOne;
-	int nodeTwo;
-	int nodes;
-	Vector<Vector<Edge>> adjacencyList;
-	Graph(int totalNodes) {
+class Graph {
+	 List<Integer>[] adjacencyList;
+	boolean cyclePresent;
+	int totalNodes;
 
+	Graph(int nodes) {
+		adjacencyList = new LinkedList[nodes+1];
+		totalNodes = nodes;
+		cyclePresent = false;
+
+		for (int i = 1; i <= totalNodes; i++) {
+			adjacencyList[i] = new LinkedList<Integer>();
+		}
+	}
+
+	void addEdge(int a, int b) {
+		adjacencyList[a].add(b);
+		adjacencyList[b].add(a);
+	}
+
+	/* In order to find a cycle in a graph, we avoid parallel-edges in the graph.
+	 * So, we send parentNode too to check that parallel edge.
+	 */
+	public void startDFS(int startNode, boolean[] visitedNodes, int parentNode) {
+		visitedNodes[startNode] = true;
+		Iterator it = adjacencyList[startNode].iterator();
+		while (it.hasNext()) {
+			int currentNode = (Integer)it.next();
+			if (visitedNodes[currentNode] == false) {
+				startDFS(currentNode, visitedNodes, startNode);
+			}
+			else {
+				if (currentNode != parentNode) {
+					cyclePresent = true;
+				}
+			}
+		}
+	}
+}
+
+public class FindCycle {
+	
+	private static void findCycle(Graph gp) {
+		boolean[] visitedNodes = new boolean[gp.totalNodes+1];
+		gp.startDFS(1, visitedNodes, -1);
+
+	}
+
+	public static void main(String[] args) {
+		Graph gp = new Graph(4);
+		gp.addEdge(1, 2);
+		gp.addEdge(1, 3);
+		gp.addEdge(1, 4);
+		//gp.addEdge(2, 3);
+		//gp.addEdge(2, 4);
+		//gp.addEdge(3, 4);
+
+		findCycle(gp);
+
+		if (gp.cyclePresent) {
+			System.out.println("Cycle found");
+		}
+		else {
+			System.out.println("Cycle not found");
+		}
 	}
 }
