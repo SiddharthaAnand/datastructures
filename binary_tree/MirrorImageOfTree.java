ac6e2591e2291734bc5cd10dcd028307b25cc0bb
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-09-14 09:28:03 +0530

    Add geeksforgeeks url question

--- binary_tree/MirrorImageOfTree.java
+++ binary_tree/MirrorImageOfTree.java
@@ -1,3 +1,7 @@
+/* URL: https://www.geeksforgeeks.org/write-an-efficient-c-function-to-convert-a-tree-into-its-mirror-tree/
+ *
+ */
+
 package binarytree;
 import  java.util.Scanner;
 
@@ -45,9 +49,10 @@ public class MirrorImageOfTree {
 		Node root = new Node(1);
 		root.left = new Node(3);
 		root.right = new Node(2);
-		//root.right.left = new Node(5);
+		root.right.left = new Node(5);
 		root.right.right = new Node(4);
-		//root.left = new Node(2);
+		root.left.left = new Node(7);
+		root.left.right = new Node(8);
 
 		inOrder(root);
 		System.out.println();
