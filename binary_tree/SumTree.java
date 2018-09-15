1d1d786f906636d4a22e91427d73b511c375bc53
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-09-15 12:26:12 +0530

    Check if a binary tree is a sumtree or not

--- binary_tree/SumTree.java
+++ binary_tree/SumTree.java
@@ -13,18 +13,38 @@ class Node {
 }
 
 public class SumTree {
-	private static int checkSumTree(Node root) {
-		if (root == null)	return true;
-		if (root.left == null && root.right == null)	return root.value;
-		int lValue = checkSumTree(root.left);
-		int rValue = checkSumTree(root.right);
-
-		if (lValue + rValue == root.value)
+	private static int sum(Node root) {
+		if (root == null)
+			return 0;
+		if (root.left == null && root.right == null)
 			return root.value;
-		return -1;
+		return sum(root.left) + sum(root.right) + root.value;
+	}
+
+	private static boolean checkSumTree(Node root) {
+		// If the root is null, then it is a sum tree.
+		if (root == null)	return true;
+		// if it is a leaf node, then it is sum-tree.
+		if (root.left == null && root.right == null)	return true;
+
+		int lValue = sum(root.left);
+		int rValue = sum(root.right);
+
+		if (lValue + rValue == root.value && checkSumTree(root.left) && checkSumTree(root.right))
+			return true;
+		return false;
 	}
 
 	public static void main(String[] args) {
-
+		Node root = new Node(26);
+		root.left = new Node(10);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(6);
+		root.right.right = new Node(3);
+		//root.left.left.right = new Node(7);
+		System.out.println();
+		System.out.println(checkSumTree(root));
+		System.out.println();
 	}
 }
