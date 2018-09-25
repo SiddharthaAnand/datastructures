dd54f6fc49cf86dd1720af634ee52601444dca21
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-09-25 23:26:30 +0530

    Check for null and empty array

--- arrays/ReplaceEveryElementWithGreatestOnRight.java
+++ arrays/ReplaceEveryElementWithGreatestOnRight.java
@@ -1,19 +1,23 @@
+/* Replace every element with the next greatest element on the right of the array.
+ */
 package arrays;
 import utilities.RandomInput;
 
 public class ReplaceEveryElementWithGreatestOnRight implements Stub {
 	// Possible in single pass of the array
 	private static void replace(int[] array) {
-		int largest = array[array.length - 1];
-		for (int idx = array.length-2; idx >= 0; idx--) {
-			int temp = array[idx];
-			array[idx] = largest;
-			if (temp > largest) {
-				largest = temp;
+		if (array != null && array.length != 0) {
+			int largest = array[array.length - 1];
+			for (int idx = array.length-2; idx >= 0; idx--) {
+				int temp = array[idx];
+				array[idx] = largest;
+				if (temp > largest) {
+					largest = temp;
+				}
 			}
+			// The rightmost element will always have the value -1
+			array[array.length-1] = -1;
 		}
-		// The rightmost element will always have the value -1
-		array[array.length-1] = -1;
 	}
 
 	public static void main(String[] args) {
