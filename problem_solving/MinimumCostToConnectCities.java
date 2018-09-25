2175cc6c1acad7931f6fcdd034c099c8e8e1b648
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-09-25 23:42:42 +0530

    add new method for cost

--- problem_solving/MinimumCostToConnectCities.java
+++ problem_solving/MinimumCostToConnectCities.java
@@ -1,7 +1,16 @@
 /* URL: https://www.geeksforgeeks.org/minimum-cost-connect-cities/
  */
 package problem_solving;
+import java.util.Scanner;
+import graphs.Graph;
 
 public class MinimumCostToConnectCities {
 	
+	private static void findMinimumCost() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
 }
