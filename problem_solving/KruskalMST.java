d4950c40df457375378901a42abc5c49894bb87e
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-10-05 22:07:42 +0530

    first state of kruskal

--- problem_solving/KruskalMST.java
+++ problem_solving/KruskalMST.java
@@ -3,10 +3,11 @@ import problem_solving.Graph;
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.Iterator;
 
 public class KruskalMST {
 
-	private static void readFile(String filename) throws FileNotFoundException {
+	private static void readFileAndCreateAdjacencyList(String filename, Graph g) throws FileNotFoundException {
 		File f = new File(filename);
 		Scanner sc = new Scanner(f);
 		while (sc.hasNext()) {
@@ -15,12 +16,23 @@ public class KruskalMST {
 			int nodeOne = edge.charAt(0) - 48;
 			int nodeTwo = edge.charAt(2) - 48;
 			System.out.println(nodeOne + " " + nodeTwo);
+			g.addEdge(nodeOne, nodeTwo);
 		}
+	}
+
+	private static void printAdjacencyList(Graph g) {
+		Iterator it = g.adjacencyList[0].iterator();
+
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter total nodes: ");
+		int totalNodes = sc.nextInt();
+		System.out.println("Enter filename of edgelist: ");
 		String filename = sc.next();
-		readFile(filename);
+		Graph g = new Graph(totalNodes);
+		readFileAndCreateAdjacencyList(filename, g);
+		printAdjacencyList(g);
 	}	
 }
