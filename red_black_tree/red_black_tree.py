592f9315bcdae938eaeb6d2a5ec21c1bcb6cf8c5
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-10-21 11:05:47 -0400

    Adds main file for the Red Black Tree python implementation

---
+++ red_black_tree/red_black_tree.py
@@ -0,0 +1,14 @@
+'''
+Basic implementation of a Red Black Tree data structure in Python.
+
+Implemented in Python 3.6
+'''
+
+class RedBlackTree():
+	pass
+
+def main():
+	print("This works!")
+
+if __name__ == '__main__':
+	main()
