dfb06e9acaa66bf2899c2758fbd16fb2908b81d9
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-10-26 08:38:34 +0530

    add method to calculate the length of LCS

--- dynamic_programming/LongestCommonSubsequence.java
+++ dynamic_programming/LongestCommonSubsequence.java
@@ -10,8 +10,28 @@ import java.io.FileNotFoundException;
 import java.io.File;
 
 public class LongestCommonSubsequence {
+	private static int max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+
 	public static int findLCS(final StringBuffer a, final StringBuffer b) {
 		int lengthLCS = -1;
+		int[][] lcs = new int[a.length()+1][b.length()+1];
+
+		for (int i = 0; i <= a.length(); i++) {
+			for (int j = 0; j <= b.length(); j++) {
+				if (i == 0 || j == 0) {
+					lcs[i][j] = 0;
+				}
+				else if (a.charAt(i-1) == b.charAt(j-1)) {
+					lcs[i][j] = lcs[i-1][j-1] + 1;
+				}
+				else {
+					lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1]);
+				}
+			}
+		}
+		lengthLCS = lcs[a.length()][b.length()];
 		return lengthLCS;
 
 	}
@@ -33,7 +53,7 @@ public class LongestCommonSubsequence {
 		// Send them as strings since they are immutable and thread-safe
 		// All immutable objects are thread-safe
 
-		System.out.println("a: " + a);
+		//System.out.println("a: " + a);
 		System.out.println("Length of the longest common subsequence: " +
 							findLCS(a, b));
 
