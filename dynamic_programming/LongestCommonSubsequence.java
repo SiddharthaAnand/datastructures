9a56acc86e5b7c28de4fa9187127f8e838ad372b
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-10-27 16:15:44 +0530

    Add method to print the subsequence

--- dynamic_programming/LongestCommonSubsequence.java
+++ dynamic_programming/LongestCommonSubsequence.java
@@ -1,6 +1,7 @@
 /* URL: https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/
  * Compare two files and find the longest common subsequence in them.
  * diff works by following this.
+ * https://www.geeksforgeeks.org/printing-longest-common-subsequence/
  */
 
 package dynamic_programming;
@@ -17,7 +18,7 @@ public class LongestCommonSubsequence {
 	public static int findLCS(final StringBuffer a, final StringBuffer b) {
 		int lengthLCS = -1;
 		int[][] lcs = new int[a.length()+1][b.length()+1];
-
+		StringBuffer result = new StringBuffer();
 		for (int i = 0; i <= a.length(); i++) {
 			for (int j = 0; j <= b.length(); j++) {
 				if (i == 0 || j == 0) {
@@ -25,6 +26,8 @@ public class LongestCommonSubsequence {
 				}
 				else if (a.charAt(i-1) == b.charAt(j-1)) {
 					lcs[i][j] = lcs[i-1][j-1] + 1;
+					//result.append(a.charAt(i-1));
+
 				}
 				else {
 					lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1]);
@@ -32,9 +35,33 @@ public class LongestCommonSubsequence {
 			}
 		}
 		lengthLCS = lcs[a.length()][b.length()];
+		//System.out.println("Common subsequence: " + result);
+		printCommonSubsequece(a, lcs);
 		return lengthLCS;
 
 	}
+
+	/* Print the longest common subsequence 
+	 */
+	public static void printCommonSubsequece(final StringBuffer a, int[][] lcs) {
+		String result = "";
+		int i = lcs.length-1, j = lcs[0].length-1;
+		while (i != 0 && j != 0) {
+			if (lcs[i][j] == lcs[i-1][j-1] + 1) {
+				result = a.charAt(i-1) + result;
+				i--;
+				j--;
+			}
+			else if (lcs[i-1][j] != lcs[i][j-1] && lcs[i][j] == lcs[i][j-1]) {
+				j--;
+			}
+			else {
+				i--;
+			}
+		}
+		System.out.println("Result made from lcs array: " + result);
+	}
+
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner sc = new Scanner(System.in);
 		String fileOne = sc.next();
@@ -47,11 +74,12 @@ public class LongestCommonSubsequence {
 		StringBuffer b = new StringBuffer();
 		sc = new Scanner(fOne);
 		while (sc.hasNextLine()) {
-			a = a.append(sc.nextLine());
+			a.append(sc.nextLine());
 		}
+		System.out.println(a);
 		sc = new Scanner(fTwo);
 		while (sc.hasNextLine()) {
-			b = b.append(sc.nextLine());
+			b.append(sc.nextLine());
 		}
 		// Send them as strings since they are immutable and thread-safe
 		// All immutable objects are thread-safe
