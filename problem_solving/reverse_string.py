4a985eec066477a798f53790d130e330bfbd3577
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-12-15 19:40:45 +0530

    Update to print the reversed string

--- problem_solving/reverse_string.py
+++ problem_solving/reverse_string.py
@@ -14,4 +14,6 @@ def reverse_string_recursive(_input, start, end, _reversed):
 	print _reversed,
 _reversed = ""
 #reverse_string("Siddhartha", 0, len("Siddhartha")-1, _reversed)
-reverse_string_recursive("Siddhartha", 0, len("Siddhartha")-1, _reversed)
+
+_string = raw_input("Enter the string to reverse:")
+reverse_string_recursive(_string, 0, len(_string)-1, _reversed)
