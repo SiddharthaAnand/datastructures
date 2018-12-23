e83fc6e65e9e725702de137322f73982b65877fb
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-12-23 21:26:18 +0530

    Add method to merge two linked lists alternately

--- linked_list/ReArrangerClass.java
+++ linked_list/ReArrangerClass.java
@@ -5,12 +5,53 @@ package linked_list;
 class Node {
 	int data;
 	Node next;
+
+	Node(int d) {
+		data = d;
+	}
 }
 
 public class ReArrangerClass {
 	
 	public static void rearrange(Node head) {
+		Node middle = findMiddle(head);
+		print(middle);
+		Node anotherList = reverse(middle.next);
+		print(anotherList);
+		middle.next = null;
+		
+		// merge the two lists alternatively
+		Node tmp1 = head.next;
+		Node tmp2 = anotherList.next;
+		while (anotherList.next != null && head.next != null) {
+			head.next = anotherList;
+			anotherList.next = tmp1;
+			head = tmp1;
+			tmp1 = tmp1.next;
+			anotherList = tmp2;
+			tmp2 = tmp2.next;
+		}
+		if (anotherList.next != null) {
+			head.next = anotherList;
+			//head = head.next;
+			//anotherList = anotherList.next;
+		}
+		if (head.next != null) {
+			tmp1 = head.next;
+			head.next = anotherList;			
+			anotherList.next = tmp1;
+		}
+	}
 
+	public static Node findMiddle(Node head) {
+		Node tortoise = head;
+		Node hare = head;
+
+		while (hare != null && hare.next != null) {
+			tortoise = tortoise.next;
+			hare = hare.next.next;
+		}
+		return tortoise;
 	}
 
 	public static void print(Node head) {
@@ -19,12 +60,25 @@ public class ReArrangerClass {
 			System.out.print(head.data + " ");
 			head = head.next;
 		}
+		System.out.println();
 	}
 
-	public static void reverse(Node head) {
+	public static Node reverse(Node head) {
+		Node prev = null;
+		Node curr = head;
+		Node nex = head.next;
 
+		while (curr != null && curr.next != null) {
+			curr.next = prev;
+			prev = curr;
+			curr = nex;
+			nex = nex.next;
+		}
+		curr.next = prev;
+		head = curr;
+		return head;
 	}
-	
+
 	public static void main(String[] args) {
 		Node head = new Node(1);
 		head.next = new Node(2);
@@ -33,6 +87,10 @@ public class ReArrangerClass {
 		head.next.next.next.next = new Node(5);
 		head.next.next.next.next.next = new Node(6);
 		head.next.next.next.next.next.next = new Node(7);
+
+		print(head);
+		rearrange(head);
+		print(head);
 
 	}
 }
