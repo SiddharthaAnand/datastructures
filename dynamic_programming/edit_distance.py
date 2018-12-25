7e24e7a2a5909f77bbf2d05b3c2865919eb03051
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-12-25 21:57:41 +0530

    Add comments, Add efficient method to remove redundant calcuations of recursive calls

--- dynamic_programming/edit_distance.py
+++ dynamic_programming/edit_distance.py
@@ -1,3 +1,28 @@
+# https://practice.geeksforgeeks.org/problems/edit-distance/0
+# Edit Distance
+
+# Optimizing the code
+def optimize_edit(S, i, T, j):
+    dist = [[x for x in range(j+1) ] for x in range(i+1)]
+
+    for i in range(i+1):
+        for j in range(j+1):
+            if i == 0:
+                dist[i][j] = j
+            elif j == 0:
+                dist[i][j] = i
+            elif S[i-1] == T[j-1]:
+                dist[i][j] = dist[i-1][j-1]
+            else:
+                dist[i][j] = 1 + min(dist[i-1][j-1], dist[i-1][j], dist[i][j-1])
+
+    return dist[len(S)][len(T)]
+
+
+
+
+# Recursive soliution for the problem which takes more time than expected.
+# Calculates sub-problems again and again
 def edit(S, i, T, j):
     if i == 0:
         return j-i
@@ -6,11 +31,19 @@ def edit(S, i, T, j):
     if S[i] == T[j]:
         return edit(S, i-1, T, j-1)
     if S[i] != T[j]:
-        return 1 + min(edit(S, i-1, T, j), edit(S, i, T, j-1))
+        return 1 + min(edit(S, i-1, T, j-1), edit(S, i-1, T, j), edit(S, i, T, j-1))
         
+# t - number of test cases
+# i, j - length of the two strings S and T
+# S needs to be converted to T
+# S, T - the two strings as input
+t = int(input())
 while (t > 0):
     t -= 1
-    i, j = [int(x) for x in input().split()]
-    S, T = [x for x in input().split()]
+    i = int(input())
+    j = int(input())
+    S = input()
+    T = input()
     print (edit(S, i-1, T, j-1))
+    print (optimize_edit(S, i, T, j))
     
