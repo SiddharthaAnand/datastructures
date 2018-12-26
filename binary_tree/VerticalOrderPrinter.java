948333dcd924bf480508a099d26d40e117fba361
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-12-26 21:11:30 +0530

    Class Added which prints nodes in vertical order

---
+++ binary_tree/VerticalOrderPrinter.java
@@ -0,0 +1,97 @@
+/* URL: https://www.geeksforgeeks.org/print-binary-tree-vertical-order-set-2/
+ * Solution: 
+ */
+
+package binary_tree;
+
+import java.util.TreeMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+/* Node structure to hold the data and the pointers */
+class Node {
+	int data;
+	Node left;
+	Node right;
+
+	Node (int d) {
+		data = d;
+		left = right = null;
+	}
+}
+
+public class VerticalOrderPrinter {
+	
+	// Need this when we need the elements to be stored in a sorted order.
+	private static TreeMap<Integer, LinkedList<Integer>> tm = new TreeMap<Integer, LinkedList<Integer>>();
+
+	static void printVerticalOrder(Node root, int dist) {
+		if (root == null)
+			return;
+
+		if (tm.get(dist) == null) {
+			LinkedList<Integer> l = new LinkedList<Integer>();
+			l.add(root.data);
+			tm.put(dist, l);
+		}
+		else {
+			LinkedList<Integer> l = tm.get(dist);
+			l.add(root.data);
+		}
+		printVerticalOrder(root.left, dist-1);
+		printVerticalOrder(root.right, dist+1);
+
+
+	}
+
+	static void printMap() {
+		System.out.println();
+		for (Map.Entry<Integer, LinkedList<Integer>> entry : tm.entrySet()) {
+			int key = entry.getKey();
+			LinkedList<Integer> l = entry.getValue();
+			Iterator it = l.iterator();
+			System.out.print("Key: " + key + " : ");
+			while (it.hasNext()) {
+				System.out.print(it.next() + " ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+	static void printVerticalOrder(Node root) {
+		int dist = 0;
+		printVerticalOrder(root, dist);
+	}
+
+	static void printInorder(Node root) {
+		if (root == null)
+			return;
+
+		printInorder(root.left);
+		System.out.print(root.data + " ");
+		printInorder(root.right);
+	}
+
+	public static void main(String[] args) {
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+		root.right.left.right = new Node(8);
+		root.right.right.left = new Node(10);
+		root.right.right.right = new Node(9);
+		root.right.right.left.right = new Node(11);
+		root.right.right.left.right.right = new Node(12);
+		
+		System.out.println();
+		printInorder(root);
+		System.out.println();
+		printVerticalOrder(root);
+		printMap();
+
+	}
+}
