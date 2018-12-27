bca9d7985802a02cbbe0bafa90600276144e27e5
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-12-27 23:27:45 +0530

    Add the sorting method in a linkedlist of 0s,1s and 2s

--- linked_list/DeleteNodeClass.java
+++ linked_list/DeleteNodeClass.java
@@ -9,5 +9,35 @@ class DeleteNodeClass
 	     del.data = del.next.data;
 	     del.next = del.next.next;
     }
+
+    /* URL: https://practice.geeksforgeeks.org/problems/given-a-linked-list-of-0s-1s-and-2s-sort-it/1/?ref=self
+     */
+    void linksort(Node head) {
+		int zero = 0, one = 0, two = 0;
+		Node tmp = head;
+		while (head != null) {
+		    if (head.value == 0) zero++;
+		    if (head.value == 1) one++;
+		    if (head.value == 2) two++;
+		    head = head.next;
+		}
+		while (tmp != null) {
+		    while (zero > 0) {
+		        zero--;
+		        tmp.value = 0;
+		        tmp = tmp.next;
+		    }
+		    while (one > 0) {
+		        one--;
+		        tmp.value = 1;
+		        tmp = tmp.next;
+		    }
+		    while (two > 0) {
+		        two--;
+		        tmp.value = 2;
+		        tmp = tmp.next;
+		    }
+		}
+	}  
 }
 
