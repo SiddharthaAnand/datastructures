047cf1b3d27611a81b6dbaa80a7c2d3b61332e3a
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-12-29 23:49:39 +0530

    Add comments for methods

--- strings/Permute.java
+++ strings/Permute.java
@@ -1,9 +1,15 @@
+/* URL: https://practice.geeksforgeeks.org/problems/permutations-of-a-given-string/0/?ref=self
+ */
+
 package strings;
 
 import java.util.*;
 
 class Permute {
 
+	/* This method does not necessarily return strings 
+	 * in alphabetical order.
+	 */
 	static void permuteString(char[] input, int i) {
 		if (i == input.length) {
 			System.out.println(input);
@@ -18,6 +24,9 @@ class Permute {
 		}
 	}
 
+	/* Utility to swap the characters since Strings
+	 * are immutable.
+	 */
 	static void swap(char[] input, int x, int y) {
 		char t = input[x];
 		input[x] = input[y];
@@ -25,6 +34,7 @@ class Permute {
 	}
 
 	public static void main(String[] args) {
+		
 		String input = "abc";
 		char[] inp = input.toCharArray();
 		permuteString(inp, 0);
