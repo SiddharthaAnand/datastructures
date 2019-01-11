fffa6b08a5fb3b03d9197eb5310667ce33f85681
Siddhartha Anand <siddharthalibra13@gmail.com>
2019-01-11 23:58:04 +0530

    Add caller method and input lines

--- arrays/IslandFinder.java
+++ arrays/IslandFinder.java
@@ -12,5 +12,16 @@ class IslandFinder {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int row = sc.nextInt();
+		int col = sc.nextInt();
+		int[][] matrix = new int[row][col];
+
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				matrix[i][j] = sc.nextInt();
+			}
+		}
+
+		countIsland(matrix, row, col);
 	}
 }
