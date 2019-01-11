11a5b229c693d5419d2df541ea21516c48cad8ab
Siddhartha Anand <siddharthalibra13@gmail.com>
2019-01-11 23:56:28 +0530

    COunt number of islands method skeleton

---
+++ arrays/IslandFinder.java
@@ -0,0 +1,16 @@
+package arrays;
+
+import java.util.Scanner;
+
+class IslandFinder {
+	
+	private static int count = 0;
+
+	private static void countIsland(int[][] matrix, int row, int col) {
+
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
+}
