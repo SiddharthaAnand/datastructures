44d8679579c0df298f524d1c833a47f7976e2e5a
Siddhartha Anand <siddharthalibra13@gmail.com>
2019-03-03 17:06:04 +0530

    Merge non-overlapping intervals

--- problem_solving/MergeNewInterval.java
+++ problem_solving/MergeNewInterval.java
@@ -1,12 +1,12 @@
 // https://www.interviewbit.com/problems/merge-intervals/
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
+
+//Definition for an interval.
+ public class Interval {
+     int start;
+     int end;
+     Interval() { start = 0; end = 0; }
+     Interval(int s, int e) { start = s; end = e; }
+ }
  */
 public class MergeNewInterval {
     static class IntervalCompare implements Comparator<Interval> {
