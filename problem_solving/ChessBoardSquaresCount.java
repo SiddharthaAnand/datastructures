288080911942dd52684c28859c8d2d3d94933b5a
Siddhartha Anand <siddharthalibra13@gmail.com>
2019-04-30 22:02:12 +0530

    Count number of rectangles in a chessboard

--- problem_solving/ChessBoardSquaresCount.java
+++ problem_solving/ChessBoardSquaresCount.java
@@ -5,6 +5,18 @@ import java.lang.*;
 import java.io.*;
 
 class ChessBoardSquaresCount {
+
+	private static int nC2(int n) {
+		return (n * (n-1)) / 2;
+	}
+
+	// Take two horizontal lines and two vertical lines 
+	// for the creation of rectangle.
+	// n+1 - because there are n+1 lines needed to create n sized rectangles.
+	private static int rectanglesCount(int n) {
+		return nC2(n+1) * nC2(n+1);
+	}
+
     private static int squaresCount(int n) {
         int result = 0;
         for (int i = 0; i < n; i++) {
@@ -21,6 +33,7 @@ class ChessBoardSquaresCount {
 		    t -= 1;
 		    int n = sc.nextInt();
 		    System.out.println(squaresCount(n));
+		    System.out.println(rectanglesCount(n));
 		}
 	}
 }
