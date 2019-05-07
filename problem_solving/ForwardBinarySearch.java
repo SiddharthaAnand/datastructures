7154ac398fb15095062b7429cc671ba5ca868afb
Siddhartha Anand <siddharthalibra13@gmail.com>
2019-05-07 20:19:39 +0530

    Modified Binary search implemented in a strictly monotonically decreasing function to find out a value

--- problem_solving/ForwardBinarySearch.java
+++ problem_solving/ForwardBinarySearch.java
@@ -1,3 +1,9 @@
+/* Question: Find the first x value where the y value is negative in a 
+ * strictly monotonically decreasing function. 
+ * Assumption: Atleast one positive value present.
+ * Given: value of n(sizeof the array) not known in advance.
+ * Solution: O(log k + log n) complexity
+ */
 import java.util.Scanner;
 
 class ForwardBinarySearch {
@@ -5,8 +11,8 @@ class ForwardBinarySearch {
 	public static int binarySearch(int[] input, int left, int right) {
 		if (left >= 0 && right >= 0 && input.length != 0) {
 			while (left <= right) {
-				int mid = right - (left + right) / 2;
-				if (input[mid] < 0 && mid-1 >= 0 && input[mid-1] >= 0) {
+				int mid = (left + right) / 2;
+				if (input[mid] < 0 && (mid-1) >= 0 && input[mid-1] >= 0) {
 					return mid;
 				}
 				else if (input[mid] > 0) {
@@ -24,18 +30,21 @@ class ForwardBinarySearch {
 		if (input.length == 1 && input[0] < 0) {
 			return 0;
 		}
-		
+
 		int jumper = 1;
-		while (jumper < input.length) {
-			if (input[jumper] > 0) {
-				jumper = jumper << 2;
+		while (jumper <= input.length) {
+			if (input[jumper-1] > 0) {
+				jumper = jumper << 1;
+				if (jumper > input.length) {
+					jumper = input.length;
+				}
 			}
 			else {
-				return binarySearch(input, jumper >> 2, jumper);
+				return binarySearch(input, jumper >> 1, jumper);
 			}
 		}
 		return -1;
-	}
+	}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
