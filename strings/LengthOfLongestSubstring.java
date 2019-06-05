ce258f5949ef8e5b5608cb3114f184becbab7668
Siddhartha Anand <siddharthalibra13@gmail.com>
2019-06-05 12:31:46 +0530

    Find the longest string which has sorted chars

--- strings/LengthOfLongestSubstring.java
+++ strings/LengthOfLongestSubstring.java
@@ -3,14 +3,37 @@
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Scanner;
 
 class LengthOfLongestSubstring {
+
+	public static int lengthOfLongestSubstringInAlphabeticalOrder(String input) {
+		int ans = 0;
+		if (input == null && input.length() == 0) {
+			return ans;
+		}
+		if (input.length() == 1) {
+			return 1;
+		}
+		for (int i = 0, j = 1; i < input.length() && j < input.length();) {
+			if (input.charAt(j-1) < input.charAt(j)) {
+				ans = Math.max(j - i + 1, ans);
+				j++;
+			}
+			else {
+				i = j;
+				j++;
+			}
+		}
+		return ans;
+	}
+
 	/* Sliding Window where the window has only unique characters in every iteration. 
 	 * Every character visit is done only once.
 	 * Time Complexity: O(n)
 	 * Space complexity: O(n)
 	 */
-	public static int lengthOfLongestSubstringContainingUniqueCharacters(StringBuilder input) {
+	public static int lengthOfLongestSubstringContainingUniqueCharacters(String input) {
 		int ans = 0;
 		if (input == null || input.length() == 0) {
 			return ans;
@@ -28,17 +51,40 @@ class LengthOfLongestSubstring {
 		return ans;
 	}
 	public static void main(String[] args) {
-		StringBuilder input = new StringBuilder("abcdabcd");
-		System.out.println(lengthOfLongestSubstringContainingUniqueCharacters(input));
-		input = new StringBuilder("aabbccdd");
-		System.out.println(lengthOfLongestSubstringContainingUniqueCharacters(input));
-		input = new StringBuilder("aaaa");
-		System.out.println(lengthOfLongestSubstringContainingUniqueCharacters(input));
-		input = new StringBuilder("");
-		System.out.println(lengthOfLongestSubstringContainingUniqueCharacters(input));
-		input = new StringBuilder("aabcd");
-		System.out.println(lengthOfLongestSubstringContainingUniqueCharacters(input));
-		input = new StringBuilder("abcdbcdef");
-		System.out.println(lengthOfLongestSubstringContainingUniqueCharacters(input));
+		System.out.println("This code prints the following: ");
+		System.out.println("1. Returns the longest substring containing unique characters.");
+		System.out.println("2. Returns the longest substring containing sorted alphabetically characters.");
+		System.out.println("3. Exit.");
+		System.out.println("Which one do you want to choose? 1/2/3");
+		
+		String input = "";
+		Scanner sc = new Scanner(System.in);
+		String ch = sc.next();
+		switch (ch) {
+			case "1":
+				System.out.println("Enter the string to be given as input: ");
+				System.out.println(lengthOfLongestSubstringContainingUniqueCharacters(sc.next()));
+				break;
+			case "2":
+				System.out.println("Enter the string to be given as input: ");
+				System.out.println(lengthOfLongestSubstringInAlphabeticalOrder(sc.next()));
+				break;
+			case "3":
+				break;
+			default:
+				break;
+		}
+		// StringBuilder input = new StringBuilder("abcdabcd");
+		// System.out.println(lengthOfLongestSubstringContainingUniqueCharacters(input));
+		// input = new StringBuilder("aabbccdd");
+		// System.out.println(lengthOfLongestSubstringContainingUniqueCharacters(input));
+		// input = new StringBuilder("aaaa");
+		// System.out.println(lengthOfLongestSubstringContainingUniqueCharacters(input));
+		// input = new StringBuilder("");
+		// System.out.println(lengthOfLongestSubstringContainingUniqueCharacters(input));
+		// input = new StringBuilder("aabcd");
+		// System.out.println(lengthOfLongestSubstringContainingUniqueCharacters(input));
+		// input = new StringBuilder("abcdbcdef");
+		// System.out.println(lengthOfLongestSubstringContainingUniqueCharacters(input));
 	}	
 }
