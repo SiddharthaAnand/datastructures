f5ae2a550ab40b755a86fcdf1db0f82598e2d858
Siddhartha Anand <siddharthalibra13@gmail.com>
2019-08-18 11:09:02 +0530

    FInd subarray with sum K - given that the input contains only positive numbers

---
+++ arrays/SubArrayWithSumK.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class SubArrayWithSumK {
+	public static void findSubArrayWithSumK(int[] arr,
+											int K) {
+		if (arr == null || arr.length == 0) {
+			return;
+		}
+		int start = 0, currSum = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			currSum += arr[i];
+			while (currSum > K && start < i) {
+				currSum -= arr[start];
+				start++;
+			}
+			if (currSum == K) {
+				System.out.println("start: " + start + " end: " + i);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		int i = 0;
+		while (i < n) {
+			arr[i] = sc.nextInt();
+			i++;
+		}
+		System.out.println("Enter the sum to be found: ");
+		int K = sc.nextInt();
+		findSubArrayWithSumK(arr, K);
+	}
+}
