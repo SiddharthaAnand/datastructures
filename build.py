0185df086d55f4256e45eb2186d807ba22a14a91
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-10-23 22:41:34 +0530

    Add some content stage name here

--- build.py
+++ build.py
@@ -2,7 +2,10 @@ import logging
 
 class Stages:
     def __init__(self, stage_name):
-        self.name = stage_name
+        self.stage_name = stage_name
+
+    def __repr__(self):
+        print("repr: [name:{0}]".format(__name__))
 
 
 class Build(Stages):
