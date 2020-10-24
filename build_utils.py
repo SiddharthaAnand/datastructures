af932e449c1d668ebd7ce4c5ad056770094952a4
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-10-24 18:59:28 +0530

    Remove redundant params| Add documentation string for the method

--- build_utils.py
+++ build_utils.py
@@ -1,11 +1,13 @@
 import os
 
 
-def get_dirs(path='.'):
+def get_dirs():
     """
-
-    :param path:
-    :return:
+    This method expects src/ to be present in the directories which want to
+    be compiled and tested.
+    It creates the paths to the files from the current working directory
+    so that it can be compiled from the current working directory.
+    :return: dictionary containing directories as keys and the files inside src/.
     """
     compilable_files = {}
     for a, b, c in os.walk(os.getcwd()):
