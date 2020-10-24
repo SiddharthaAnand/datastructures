a2415c8d3de1d6da8e7f90f37169fdbf90444cb7
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-10-24 18:27:58 +0530

    Add skeleton and structure to the build class

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 import logging
 
+
 class Stages:
     def __init__(self, stage_name):
         self.stage_name = stage_name
@@ -7,20 +8,57 @@ class Stages:
     def __repr__(self):
         print("repr: [name:{0}]".format(__name__))
 
+    def run(self):
+        pass
+
 
 class Build(Stages):
-    pass
+    __name__ = 'build'
+    def __init__(self, stage):
+        super().__init__(__name__)
+
+    def run(self):
+        pass
 
 
 class Test(Stages):
-    pass
+    def __init__(self):
+        __name__ = 'test'
+        super().__init__(__name__)
 
+    def run(self):
+        pass
 
 class Compile(Stages):
-    pass
+    __name__ = 'compile'
+
+    def __init__(self):
+        super().__init__(stage_name=__name__)
+
+    def run(self):
+        #TODO Get the list of directories containing the py/java files
+        #TODO Compile/Run then one by one
+        #TODO Use a logger
+        pass
 
 
 class StageRunner:
-    def __init__(self, **kwargs):
-        pass
+    def __init__(self, *args):
+        """
+        :param kwargs: dictionary of stages inheriting Stages class
+        """
+        assert args is not  None and len(args) != 0
+        self.stages = args
 
+    def run(self):
+        for stage in self.stages:
+            stage.run()
+
+    def validate(self):
+        for stage in self.stages:
+            stage.run()
+
+
+if __name__ == '__main__':
+    stage_runner = StageRunner()
+    stage_runner.run()
